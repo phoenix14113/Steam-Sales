69e1b204cf071dfe18aaa8375dddfba5b6757fb7
Adam Taylor <adamt@wavecable.com>
2023-03-09 15:06:28 -0800

    removing history

---
+++ js/NavBar.js
@@ -0,0 +1,84 @@
+'use strict'
+
+function addLogo () {
+  // create a span that has the logo attached to it
+  const span = document.createElement('span')
+  span.classList.add('logo')
+  span.ariaHidden = 'true'
+  span.textContent = '\xa0'
+  return span
+}
+
+function addlink (path, text, span = null) {
+  // Have the input text link to the path when clicked
+  const a = document.createElement('a')
+  a.classList.add('link')
+  const content = document.createTextNode(text)
+  a.href = path
+
+  // If a span is passed in put it in before adding the text
+  if (span) {
+    a.appendChild(span)
+  }
+  a.appendChild(content)
+  return a
+}
+
+function createLI (path, text, addLogoElement = false) {
+  let span = null
+
+  // if this function starts comes with a true addLogoElement then
+  // a logo will be added to the a element before its text
+  if (addLogoElement) {
+    span = addLogo()
+  }
+  // create a element
+  const a = addlink(path, text, span)
+
+  // turn the a element into a list entry
+  const li = document.createElement('li')
+  li.appendChild(a)
+
+  // add it to the navbar
+  li.addEventListener('click', () => {
+    document.location.href = path
+  })
+  document.querySelector('ul').appendChild(li)
+}
+
+function createNavBar () {
+  // add homepage navigation link
+  createLI('./index.html#', 'Main\xa0Page', true)
+
+  // add Why This Site navigation link
+  createLI('./index.html#column-two', 'Why\xa0This\xa0Site', false)
+
+  // add Sales Table navigation link
+  createLI('./GamesTable.html', 'Sales\xa0Table', false)
+
+  // add About Us navigation link
+  createLI('./AboutTheTeam.html', 'About\xa0Us', false)
+}
+
+function deleteNavBar () {
+  const ul = document.querySelector('ul')
+  // remove everything that is not the menu li element
+  while (ul.lastElementChild !== ul.firstElementChild) {
+    ul.removeChild(ul.lastElementChild)
+  }
+}
+
+let showingNavBar = false
+
+document.querySelector('li#menu').addEventListener('click', () => {
+  // create interactive menu li entry
+  if (!showingNavBar) {
+    // show the nav bar when pressed if the navbar isn't present
+    showingNavBar = true
+    createNavBar()
+  } else {
+    // remove the nav bar when pressed if the navbar isn't present
+    showingNavBar = false
+    deleteNavBar()
+  }
+})
---
+++ js/TableData.js
@@ -0,0 +1,168 @@
+'use strict'
+// https://cors-anywhere.herokuapp.com/ has been prepended to each API link
+// so that the link redirects through a proxy server to avoid CORS errors
+// that the steam API throws for not being a secure access.
+let progressNeeded = 0
+let currentProgress = 0
+function percentConverter (value) {
+  return String((value * 100).toFixed(0))
+}
+
+function createProgressBar () {
+  const progressBar = document.createElement('div')
+  progressBar.classList.add('progress-bar')
+  progressBar.role = 'progressbar'
+  progressBar.setAttribute('aria-valuenow', '0')
+  progressBar.setAttribute('aria-valuemin', '0')
+  progressBar.setAttribute('aria-valuemax', '100')
+  progressBar.style.width = '0%'
+
+  const progressContainer = document.createElement('div')
+  progressContainer.classList.add('progress')
+  progressContainer.appendChild(progressBar)
+
+  document.querySelector('#before-table').appendChild(progressContainer)
+}
+
+function renderItems (game) {
+  const objectKey = Object.keys(game)
+  // if the game in question has been loaded successfully
+  // also remove games that don't have sales data that aren't free
+  if (game[objectKey[0]].success && (game[objectKey[0]].data.is_free || game[objectKey[0]].data.price_overview)) {
+    // collect game data from a game object
+    const gameName = game[objectKey[0]].data.name
+    const gameImg = game[objectKey[0]].data.header_image
+    const gameWebPage = "https://store.steampowered.com/app/" + game[objectKey[0]].data.steam_appid
+    let gamePrice = null
+    let gameDiscount = null
+    // change print format if the game is free
+    if (!game[objectKey[0]].data.is_free) {
+      gamePrice = game[objectKey[0]].data.price_overview.final_formatted
+      gameDiscount = game[objectKey[0]].data.price_overview.discount_percent
+    } else {
+      gamePrice = 'Free'
+      gameDiscount = 'Free'
+    }
+    if (gameDiscount !== 0) {
+      // create a row with the game information
+      const img = document.createElement('img')
+      img.src = gameImg
+      const imageColumn = document.createElement('td')
+      imageColumn.appendChild(img)
+
+      const link = document.createElement('a')
+      link.href = gameWebPage
+      link.textContent = gameName
+
+      const NameColumn = document.createElement('td')
+      NameColumn.appendChild(link)
+
+      const PriceColumn = document.createElement('td')
+      PriceColumn.textContent = gamePrice
+
+      const DiscountColumn = document.createElement('td')
+      DiscountColumn.textContent = gameDiscount
+
+      if (gameDiscount !== 'Free') {
+        DiscountColumn.textContent += '%'
+      }
+      // add the row to the table
+      const tr = document.createElement('tr')
+      tr.appendChild(imageColumn)
+      tr.appendChild(NameColumn)
+      tr.appendChild(PriceColumn)
+      tr.appendChild(DiscountColumn)
+      document.querySelector('tbody').appendChild(tr)
+    }
+  }
+  currentProgress += 1
+  document.querySelector('.progress-bar').setAttribute('aria-valuenow', percentConverter(currentProgress / progressNeeded))
+  document.querySelector('.progress-bar').style.width = percentConverter(currentProgress / progressNeeded) + '%'
+}
+
+const URL_GAME_ID_TEMPLATE = 'https://cors-anywhere.herokuapp.com/http://store.steampowered.com/api/appdetails?appids={gameId}'
+
+function fetchItems (gamesList) {
+  // use the steam id from the profile to get the actual game data
+  document.querySelector('tbody').innerHTML = ''
+
+  // throw an error if there are no games in their library
+  if (gamesList.response.games === undefined) {
+    renderError(new Error("There don't appear to be any games in this library."))
+    return
+  }
+
+  // show interactive progress
+  progressNeeded = gamesList.response.games.length
+  currentProgress = 0
+  document.querySelector('#before-table').innerHTML = ''
+  createProgressBar()
+
+  // collect each game from their library and render it
+  gamesList.response.games.forEach((element) => {
+    const url = URL_GAME_ID_TEMPLATE.replace('{gameId}', element.appid)
+    const promise = fetch(url).then((response) => {
+      return response.json()
+    }).then(renderItems)
+      .catch(renderError)
+    return promise
+  })
+}
+
+const URL_USER_ID_TEMPLATE = 'https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=ReplaceMe&steamid={steamId}'
+function fetchGameList (steamId) {
+  // collect all games from a user's library
+  const url = URL_USER_ID_TEMPLATE.replace('{steamId}', steamId)
+  const promise = fetch(url).then((response) => {
+    return response.json()
+  }).then(fetchItems)
+    .catch(renderError)
+  return promise
+}
+
+// run the program to have an initial population of games on the screen
+fetchGameList('76561198239932484')
+
+document.querySelector('#search').addEventListener('click', (event) => {
+  // collect SteamID
+  event.preventDefault()
+  event.stopPropagation()
+  if (/^\d+$/.test(document.querySelector('input').value)) {
+    // if the input value is a number exclusively go straight to collecting
+    // the library
+    fetchGameList(document.querySelector('input').value)
+  } else {
+    // if there is a string it means that a custom url was entered which means
+    // that the SteamId must be decoded from the url
+    const URL_USER_ID_TEMPLATE = 'https://cors-anywhere.herokuapp.com/http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=ReplaceMe&vanityurl={vanityurl}'
+    const url = URL_USER_ID_TEMPLATE.replace('{vanityurl}', document.querySelector('input').value)
+    const promise = fetch(url).then((response) => {
+      return response.json()
+    }).then((data) => { fetchGameList(data.response.steamid) }).catch(renderError)
+    return promise
+  }
+})
+
+function renderError (error) {
+  // make an error message element
+  const p = document.createElement('p')
+  p.classList.add('alert')
+  p.classList.add('alert-danger')
+  console.log(error.message)
+  if (error.message === 'Unexpected token \'S\', "See /corsd"... is not valid JSON' || error.message === 'JSON.parse: unexpected character at line 1 column 1 of the JSON data') {
+    // error message for not accepting the proxy services
+    const a = document.createElement('a')
+    a.href = 'https://cors-anywhere.herokuapp.com/corsdemo'
+    a.textContent = 'Please click this link and request temporary access to use this page. Then come back to this page and refresh the page.'
+    p.appendChild(a)
+    p.style.textDecoration = 'underline'
+  } else if (error.message === "There don't appear to be any games in this library.") {
+    // error for no games owned
+    p.textContent = error.message
+  } else {
+    // error for not putting valid SteamId
+    p.textContent = 'An error occur while trying to access this SteamID!'
+  }
+  document.querySelector('tbody').innerHTML = ''
+  document.querySelector('tbody').appendChild(p)
+}
